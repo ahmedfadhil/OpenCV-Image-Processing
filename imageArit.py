4e0c7a2757dfc9e8fe1c9b481d361545a0dd2697
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-11-30 22:49:01 +0100

    image analysis

---
+++ imageArit.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+
+img1 = cv2.imread('3D-Matplotlib.png')
+img2 = cv2.imread('mainsvmimage.png')
+
+# add = img1 + img2
+# added pixel values
+# add = cv2.add(img1,img2)
+# adding weighted values
+# weighted = cv2.addWeighted(img1, 0.6, img2, 0.4, 0)
+# cv2.imshow('weighted', weighted)
+# cv2.waitKey(0)
+# cv2.destroyAllWindows()
+
+
