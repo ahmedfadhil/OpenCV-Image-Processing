1d50773140e5ca2219c97a144bff9eb995e933ad
ahmedfadhil <ahmed.fadhil@alumni.emu.edu.tr>
2017-12-03 12:24:19 +0100

    using blur in image/video analysis

--- img_filter.py
+++ img_filter.py
@@ -11,8 +11,18 @@ while (1):
 
     mask = cv2.inRange(hsv, lower_red, upper_red)
     res = cv2.bitwise_and(frame, frame, mask=mask)
+
+    kernel = np.ones((15, 15), np.float32) / 225
+    # smoothed = cv2.filter2D(res, -1, kernel)
+    blur = cv2.GaussianBlur(res, (15, 15), 0)
+    medianblur = cv2.medianBlur(res, 15)
+    bilateral = cv2.bilateralFilter(res, 15, 75, 75)
     cv2.imshow('frame', frame)
     cv2.imshow('mask', mask)
+    # cv2.imshow('smoothed', smoothed)
+    cv2.imshow('blur', blur)
+    cv2.imshow('medianblur', medianblur)
+    cv2.imshow('bilateral', bilateral)
     cv2.imshow('res', res)
 
     k = cv2.waitKey(5) & 0xFF
